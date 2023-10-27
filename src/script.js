ae855837fd1a6cadf0fedbf2707657d8a9b6d607
Yash Suresh <yashsuresh27@gmail.com>
2023-10-27 12:40:21 +0100

    feat: added a very basic toggle button and refactored script.js

--- src/script.js
+++ src/script.js
@@ -1,42 +1,66 @@
 const ctx = document.getElementById('myChart');
+
+const chartType = 'line';
+//chartJS chart type
+
+const chartData = 
+{
+  labels: 
+  ['January', 'February', 'March', 'April', 'May', 'June'],
   
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-        datasets: [{
-          label: '1 rep max (KG)',
-          data: [175, 175, 200, 220, 250, 300],
-          borderWidth: 5
-        }]
-      },
-      options: {
-        scales: {
-          
-          x: {
-            beginAtZero: false,
-            title: {
-              display: true,
-              text: 'Months',
-              font: {
-                size: 15
-              }
-            }
+  datasets: 
+  [
+    {
+      label: '1 rep max (KG)',
+      data: [175, 175, 200, 220, 250, 300],
+      borderWidth: 5
+    }
+  ]
+};
 
-          },
-
-          y: {
-            beginAtZero: false,
-            title: {
-              display: true,
-              text: 'Weight (KG)',
-              font: {
-                size: 15
-              }
-            }
-          }
-          
-
+const chartOptions = 
+{
+  scales: 
+  {
+    x: 
+    {
+      beginAtZero: false,
+      title: 
+      {
+        display: true,
+        text: 'Months',
+        font: 
+        {
+          size: 15
         }
       }
-    });
+
+    },
+
+    y: 
+    {
+      beginAtZero: false,
+      title: 
+      {
+        display: true,
+        text: 'Weight (KG)',
+        font: 
+        {
+          size: 15
+        }
+      }
+    }
+    
+
+  }
+}
+
+
+const chart = new Chart
+(ctx, 
+  {
+      type: 'line',
+      data: chartData,
+      options: chartOptions
+    }
+);
